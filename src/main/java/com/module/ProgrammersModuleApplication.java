b5955910ed4ab82772d5eee7d0cf6b5faf29594b
heojungseok <tjrwjdgj@gmail.com>
2026-09-18 21:35:33 +0900

    chore: Spring Boot 및 PostgreSQL 개발 환경 초기 설정

---
+++ src/main/java/com/module/ProgrammersModuleApplication.java
@@ -0,0 +1,13 @@
+package com.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammersModuleApplication.class, args);
+    }
+
+}
